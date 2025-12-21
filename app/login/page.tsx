b67d5e660593460dd0a13fca7018a9c16f1dcd20
Daniel Wang <dhw72@cornell.edu>
2025-12-20 22:28:37 -0500

    add text boxes

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,7 +4,9 @@ export default function LoginPage() {
     return <main>
         <h1>Login Page</h1>
         <h1>Username</h1>
+        <textarea></textarea>
         <h1>Password</h1>
+        <textarea></textarea>
         <Login />
         </main>
 }
