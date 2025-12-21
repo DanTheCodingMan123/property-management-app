7c1d94fdbd52853ace0c5ce39e9beedc0b78a353
brookeye <by239@cornell.edu>
2025-12-21 17:11:05 -0500

    Add Gaegu font and apply to login page

--- app/components/Login.tsx
+++ app/components/Login.tsx
@@ -3,15 +3,15 @@
 
 export function LoginButton() {
     return (<div className="flex items-center justify-center">
-        <button className = "font-serif text-lg pt-10" onClick = {() => alert("Login clicked")}>Login</button>
+        <button className = "font-gaegu text-lg pt-10" onClick = {() => alert("Login clicked")}>Login</button>
     </div>)
 }
 
 
 export function LoginInput (props: {label: string, type?: string}) {
     return (
-        <label className="flex font-serif items-center justify-center text-lg gap-3 pb-10"> {props.label}
-            <input className = "outline-solid rounded-lg" type={props.type || 'text'}></input>
+        <label className="flex font-gaegu items-center justify-center text-lg gap-3 pb-10"> {props.label}
+            <input className = "outline-solid rounded-lg font-gaegu px-3 py-2" type={props.type || 'text'}></input>
         </label>
     )
 
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,7 +2,7 @@ import {LoginButton, LoginInput} from "../components/Login"
 
 export default function LoginPage() {
     return <main>
-        <label className="flex font-serif items-center justify-center p-10 text-2xl tracking-widest">Login Page</label>
+        <label className="flex font-gaegu items-center justify-center p-10 text-[64px] tracking-widest">Login</label>
         <LoginInput label="Username" type = "email" />
         <LoginInput label="Password" type = "password" />
         <LoginButton />
