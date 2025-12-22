568dfe91c1bdd06fa2e03ae2dbe1f465fa838b47
Daniel Wang <dhw72@cornell.edu>
2025-12-22 00:09:39 -0500

    Add title and reformat code

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,30 @@
 import {ForgotPasswordButton, LoginButton, LoginInput, SignUpButton} from "../components/Login"
+import Image from "next/image";
 
-export default function LoginPage() {
+
+
+function Title() {
+
     return (
-        <main className="min-h-screen flex items-center justify-center">
-            <div className="
+        <header className="flex items-center gap-3 ">
+            <Image src="/logo.png" alt="Logo" width={55} height={68}/>
+            <h1 className="
+                m-0
+                text-center
+                font-gaegu 
+                pl-2
+                pt-2
+                text-[39px]
+                leading-none
+                tracking-normal 
+                text-[#456882]" > Chicken Coop</h1>
+        </header>
+    )
+}
+
+
+function LoginBox() {
+    return (<div className="
                 w-[400px]
                 bg-[#D9D9D9]
                 rounded-[10px]
@@ -30,7 +51,22 @@ export default function LoginPage() {
             <LoginInput default="password" type = "password" />
             <LoginButton />
             <ForgotPasswordButton />
+            </div>)
+       
+}
+
+
+export default function LoginPage() {
+    return (
+        <main className="min-h-screen">
+            <div className="relative">
+                <Title />
             </div>
+            
+            <div className="flex pt-5 items-center justify-center">
+                <LoginBox />
+            </div>
+            
         </main>
     );
 }
