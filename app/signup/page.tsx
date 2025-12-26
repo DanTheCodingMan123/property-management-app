1b7a3b3aec727c0b5b578b7c15dae7eb219d3b3a
Kyleena Xin <kyleenaxin2013@gmail.com>
2025-12-26 09:06:42 -0600

    Creating page.tsx and components for signup

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,7 +1,31 @@
+import {
+     LoginBox
+} from "./components/Signup";
+import Image from "next/image";
 
-
-
+function Title() {
+     return (
+          <header className="flex items-center gap-3 p-3 ">
+               <Image src="/logo.png" alt="Logo" width={50} height={65} />
+               <h1
+                    className="
+                m-0
+                text-center
+                font-gaegu 
+                pl-1
+                pt-2
+                text-[39px]
+                leading-none
+                tracking-normal 
+                text-[#456882]"
+               >
+                    {" "}
+                    Chicken Coop
+               </h1>
+          </header>
+     );
+}
 
 export default function SignUpPage() {
-    return (<h1> Sign Up Page </h1>);
+    return (<h1> Hello World </h1>);
 }
